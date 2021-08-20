dba95c8bf9ba27c9a7cbe3ba40838861a40a54e9
Wawan <wawan.setyono@gmail.com>
2021-08-20 10:56:00 +0700

    first commit

---
+++ testSelenium.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="D:/Source/Free/chromedriver_win32/chromedriver.exe")
+driver.maximize_window();
+
+driver.get("http://localhost/form.php")
+
+# mengambil kolom
+driver.find_element_by_name("nip").send_keys("12121212")
+driver.find_element_by_name("nama").send_keys("wawan")
+driver.find_element_by_name("nik").send_keys("0012020")
+driver.find_element_by_name("alamat").send_keys("legok")
+driver.find_element_by_name("perusahaan").send_keys("mamat")
+driver.find_element_by_name("tanggal").send_keys("10/01/2020")
+driver.find_element_by_name("divisi").send_keys("hrd")
+driver.find_element_by_name("posisi").send_keys("staff")
+driver.find_element_by_name("gaji").send_keys("1200000")
+driver.find_element_by_name("atasan").send_keys("tri")
+driver.find_element_by_name("submit").click()
+
+driver.close()
+driver.quit()
+print("Test Selesai...")
